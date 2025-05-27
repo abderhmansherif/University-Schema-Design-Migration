2541aac6b58c443ef5d52c508cab8444ee80283e
Abdelrhman Sherif <156851797+abderhmansherif@users.noreply.github.com>
2025-05-27 21:23:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using App.config;
+using App.Entites;
+using App.Data;
+using System.IO.Compression;
+
+namespace App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new AppDbContext())
+            {
+                var instructor = context.Instructors.Include(x =>x.Office).Include(x => x.Course).FirstOrDefault();
+
+                System.Console.WriteLine($"{instructor!.InstructorName} tech {instructor.Course.CourseName} in {instructor.Office.OfficeName}");
+            }   
+        }
+    }
+}
